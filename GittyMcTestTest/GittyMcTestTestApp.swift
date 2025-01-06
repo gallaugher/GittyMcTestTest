b3642eac2cefb80b135daba0c89ef83930ba8bc8
gallaugher <john.gallaugher@bc.edu>
2025-01-06 11:46:09 -0500

    Initial Commit

---
+++ GittyMcTestTest/GittyMcTestTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittyMcTestTestApp.swift
+//  GittyMcTestTest
+//
+//  Created by John Gallaugher on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GittyMcTestTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
